619cd3d992856f05064a52f39f9d3de170a2136f
Somayeh <mail.somayeh@gmail.com>
2018-02-26 17:38:13 +0000

    adding deduct method

--- lib/oyestercard.rb
+++ lib/oyestercard.rb
@@ -12,4 +12,8 @@ class Oyestercard
     @balance += amount
   end
 
+  def deduct(amount)
+    @balance -= amount
+  end
+
 end
--- spec/oyestercard_spec.rb
+++ spec/oyestercard_spec.rb
@@ -19,4 +19,14 @@ describe Oyestercard do
     subject.top_up(maximum_balance)
     expect{ subject.top_up 1 }.to raise_error " Maximum balance of #{maximum_balance} exceeded "
   end
+
+  describe "#deduct" do
+    it 'responds to deduct method' do
+      expect(subject).to respond_to(:deduct).with(1).argument
+    end
+    it 'deduct amount from balance' do
+      subject.top_up(10)
+      expect {subject.deduct(1) }.to change { subject.balance }.by(-1)
+    end
+  end
 end
