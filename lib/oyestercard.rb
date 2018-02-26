c72b4ea463236141a04d525fcbf37470c36114b9
Somayeh <mail.somayeh@gmail.com>
2018-02-26 16:21:13 +0000

    adding top_up method and test

--- lib/oyestercard.rb
+++ lib/oyestercard.rb
@@ -1,5 +1,13 @@
 class Oyestercard
-  def balance
-    0
+
+  attr_reader :balance
+
+  def initialize
+    @balance = 0
   end
+
+  def top_up(amount)
+    @balance += amount
+  end
+  
 end
--- spec/oyestercard_spec.rb
+++ spec/oyestercard_spec.rb
@@ -4,4 +4,14 @@ describe Oyestercard do
   it 'has a balance of zero' do
     expect(subject.balance).to eq(0)
   end
+
+  describe "#top_up" do
+    it 'responds to top_up method' do
+      expect(subject).to respond_to(:top_up).with(1).argument
+    end
+  end
+
+  it 'top up the balance' do
+    expect { subject.top_up(1) }.to change { subject.balance }.by(1)
+  end
 end
