3fbb30361b2c8fa7db134b36b24ad763aec42dfd
Somayeh <mail.somayeh@gmail.com>
2018-02-27 13:22:54 +0000

    checking minimum fare and charging for the journey

--- lib/oyestercard.rb
+++ lib/oyestercard.rb
@@ -1,10 +1,12 @@
 class Oyestercard
 
-  attr_reader :balance
+  attr_reader :balance, :journey_status
   MAXIMUM_BALANCE = 90
+  MINIMUM_FARE = 1
 
   def initialize
     @balance = 0
+    @journey_status = :not_in_transit
   end
 
   def top_up(amount)
@@ -12,6 +14,21 @@ class Oyestercard
     @balance += amount
   end
 
+  def touch_in
+    raise "you dont have enough credit" if @balance < MINIMUM_FARE
+    @journey_status = :in_transit
+  end
+
+  def touch_out
+    deduct(MINIMUM_FARE)
+    @journey_status = :not_in_transit
+  end
+
+  def in_journey?
+    journey_status == :in_transit
+  end
+
+  private
   def deduct(amount)
     @balance -= amount
   end
--- spec/oyestercard_spec.rb
+++ spec/oyestercard_spec.rb
@@ -1,6 +1,9 @@
 require 'oyestercard'
 
 describe Oyestercard do
+
+  maximum_balance = Oyestercard::MAXIMUM_BALANCE
+  minimum_fare = Oyestercard::MINIMUM_FARE
   it 'has a balance of zero' do
     expect(subject.balance).to eq(0)
   end
@@ -15,18 +18,61 @@ describe Oyestercard do
     expect { subject.top_up(1) }.to change { subject.balance }.by(1)
   end
   it 'raise an error when try to top_up more then balance limit' do
-    maximum_balance = Oyestercard::MAXIMUM_BALANCE
     subject.top_up(maximum_balance)
     expect{ subject.top_up 1 }.to raise_error " Maximum balance of #{maximum_balance} exceeded "
   end
+  # because we set deduct method to br private, we dont need to test this method anymore and we should remove it
+  # describe "#deduct" do
+  #   it 'responds to deduct method' do
+  #     expect(subject).to respond_to(:deduct).with(1).argument
+  #   end
+  #   it 'deduct amount from balance' do
+  #     subject.top_up(minimum_fare)
+  #     expect {subject.deduct(1) }.to change { subject.balance }.by(-1)
+  #   end
+  # end
 
-  describe "#deduct" do
-    it 'responds to deduct method' do
-      expect(subject).to respond_to(:deduct).with(1).argument
+  describe "#touch_in" do
+    it 'responds to touch_in' do
+      expect(subject).to respond_to(:touch_in)
     end
-    it 'deduct amount from balance' do
-      subject.top_up(10)
-      expect {subject.deduct(1) }.to change { subject.balance }.by(-1)
+    it 'shows the status when you touch in' do
+      subject.top_up(minimum_fare)
+      expect{ subject.touch_in }.to change { subject.journey_status }.to(:in_transit)
+    end
+    it 'checking minimum balance on touch in' do
+      expect{ subject.touch_in }.to raise_error "you dont have enough credit"
     end
   end
+
+  describe "#touch_out" do
+    it 'responds to touch_out' do
+      expect(subject).to respond_to(:touch_out)
+    end
+    it 'shows the status when you touch_out' do
+      subject.top_up(minimum_fare)
+      subject.touch_in
+      expect{ subject.touch_out }.to change { subject.journey_status }.to(:not_in_transit)
+    end
+    it 'charging for the journey' do
+      expect{ subject.touch_out }.to change { subject.balance }.by(-minimum_fare)
+    end
+  end
+
+  describe "#in_journey?" do
+    it 'responds to in_journey?' do
+      expect(subject).to respond_to(:in_journey?)
+    end
+    it 'shows the status when you are in journey' do
+      subject.top_up(minimum_fare)
+      subject.touch_in
+      expect(subject.in_journey?).to eq(true)
+    end
+    it 'shows the status when you are not in journey' do
+      subject.touch_out
+      expect(subject.in_journey?).to eq(false)
+    end
+  end
+
+
 end
